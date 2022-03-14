f0491248281d074c9b3beb9069a541a8ad22d877
Hart <Hartvigen@gmail.com>
2022-03-14 09:25:33 +0100

    Fix Selv Assignment

--- Smells/CodeSmellExamples/SelfAssignment.cs
+++ Smells/CodeSmellExamples/SelfAssignment.cs
@@ -2,6 +2,32 @@
 {
     public class SelfAssignment
     {
-        
+        public string SelfAssignmentGood()
+        {
+            string a = "";
+            string x = "Played College Ball you know";
+            string y = "Could have gone pro if I hadn't joined the navy";
+            string z = "Nanomachines Son";
+
+            a = z;
+            x = x;
+            y = y;
+            z = z;
+
+            return a + x + y + z;
+        }
+
+        public string SelfAssignmentSmell()
+        {
+            string a = "";
+            string x = "Played College Ball you know ";
+            string y = "Could have gone pro if I hadn't joined the navy ";
+            string z = "Nanomachines Son ";
+
+
+            a = z;
+            
+            return a + x + y + z;
+        }
     }
 }
