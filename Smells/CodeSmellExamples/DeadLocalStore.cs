2c3de55c3a9b4d603cfb41c2633800b07ec5db26
Hart <Hartvigen@gmail.com>
2022-03-14 09:17:44 +0100

    Add Dead Local Store smell

---
+++ Smells/CodeSmellExamples/DeadLocalStore.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Smells.CodeSmellExamples
+{
+    public class DeadLocalStore
+    {
+        public double DeadLocalStoreGood(double radius)
+        {
+            double pi = 3.14;
+            
+            return (pi * Math.Pow(2, radius));
+        }
+        
+        public double DeadLocalStoreSmell(double radius)
+        {
+            double pi = 3.14;
+            double notPi = 4.13;
+            int cousinsAgeInMonths = 146;
+            string niceGreeting = "Hellow World";
+            
+            return (pi * Math.Pow(2, radius));
+        }
+    }
+}
