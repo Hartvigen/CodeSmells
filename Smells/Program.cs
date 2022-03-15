510c3ca8f05fa897f0d6fd3382aca2047b85dca1
Badgy <jonaskhansen@gmail.com>
2022-03-15 12:09:13 +0100

    add args class and re-do main

--- Smells/Program.cs
+++ Smells/Program.cs
@@ -1,19 +1,81 @@
 ﻿using System;
 using Smells.CodeSmellExamples;
+using Smells.CodeSmellDispatch;
 
 namespace Smells
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args1)
         {
-            ContactInfo info = new ContactInfo();
+            Args args = new Args(args1);
+            Dispatcher Dispatch = new Dispatcher();
 
-            for (int x = 0; x < 100000000; x++)
+            Dispatch.DispatchCodeSmell(args.getSmell(), args.getVariant());
+        }
+    }
+
+    class Args
+    {
+        private string[] Choices = {
+            "dead-local-store",
+            "duplicate-code",
+            "feature-envy",
+            "god-class",
+            "long-method",
+            "self-assignment",
+            "short-circuit",
+            "type-checking"
+        };
+
+        private string Smell;
+        private string Variant;
+
+        public string getSmell() { return Smell; }
+        public string getVariant() { return Variant; }
+
+        public Args(string[] args)
+        {
+            if (args.Length == 0)
             {
-                FullUser envy = new FullUser();
-                string y  = envy.GetFullAddress();
+                Console.WriteLine("Error: no arguments detected, exiting");
+                HelpText();
+                System.Environment.Exit(1);
+            } 
+            else if (args.Length == 1)
+            {
+                if (args[0] == "help") HelpText();
+                else 
+                {
+                    Console.WriteLine("Error: insufficient amount of arguments detected, exiting");
+                    HelpText();
+                    System.Environment.Exit(1);
+                }
+            } 
+            else if (args.Length == 2)
+            {
+                Smell = args[0];
+                Variant = args[1];
+
+                if (Variant != "bad" || Variant != "good")
+                {
+                    Console.WriteLine("Variant \"" + Variant + "\" unrecognized, defaulting to good");
+                    Variant = "good";
+                }
             }
+        }
+
+        public void HelpText()
+        {
+            Console.WriteLine("### CodeSmellExamples help text ###");
+            Console.WriteLine("");
+            Console.WriteLine("Usage: Smells <code-smell> <variant>");
+            Console.WriteLine("");
+            Console.WriteLine("\t\thelp\t\tdisplay this help text");
+            Console.WriteLine("\t\tcode-smell\tthe code smell to run");
+            Console.WriteLine("\t\tvariant\t\tthe variant of the code smell; 'good' or 'bad' [default: good]");
+            Console.WriteLine("");
+            Console.WriteLine("Available smells: " + "{ " + String.Join(", ", Choices) + " }");
         }
     }
 }
