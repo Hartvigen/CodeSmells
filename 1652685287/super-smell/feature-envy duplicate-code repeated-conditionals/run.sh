8b1f6de7dcc1090cea96a2eabd4dd205697cc8d5
benchy-data <cs-21-pt-9-01@hotmail.com>
2022-05-18 07:09:47 +0000

    mixed test results

---
+++ 1652685287/super-smell/feature-envy duplicate-code repeated-conditionals/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+/home/usr/Downloads/CodeSmells/binary/Smells super-smell feature-envy duplicate-code repeated-conditionals
+#!/bin/bash
+/home/usr/Downloads/CodeSmells/binary/Smells super-smell feature-envy duplicate-code repeated-conditionals
