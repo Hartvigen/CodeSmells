732abf52442ae280ae15c31571f8526f040edd0e
benchy-data <cs-21-pt-9-01@hotmail.com>
2022-05-27 07:16:44 +0000

    rerun tests

--- fixed-super-smells.sh
+++ fixed-super-smells.sh
@@ -3,10 +3,12 @@
 # smells to run
 SMELLS=('super-smell')
 # variants to run for each smell
-VARIANTS=('feature-envy parameter-by-value' 'feature-envy duplicate-code' 'feature-envy repeated-conditionals' 'feature-envy short-circuit' 'parameter-by-value duplicate code' 'parameter-by-value repeated-conditionals' 'parameter-by-value short-circuit' 'duplicate-code repeated-conditionals' 'duplicate-code short-circuit' 'repeated-conditionals short-circuit' 'feature-envy parameter-by-value duplicate-code' 'feature-envy parameter-by-value repeated-conditionals' 'feature-envy parameter-by-value short-circuit' 'feature-envy duplicate-code repeated-conditionals' 'feature-envy duplicate-code repeated-conditionals' 'feature-envy duplicate-code short-circuit' 'feature-envy repeated-conditionals short-circuit' 'parameter-by-value duplicate-code repeated-conditionals' 'parameter-by-value duplicate-code short-circuit' 'parameter-by-value repeated-conditionals short-circuit' 'duplicate-code repeated-conditionals short-circuit' 'feature-envy parameter-by-value duplicate-code repeated-conditionals' 'feature-envy parameter-by-value duplicate-code short-circuit' 'feature-envy parameter-by-value repeated-conditionals short-circuit' 'feature-envy duplicate-code repeated-conditionals short-circuit' 'parameter-by-value duplicate-code repeated-conditionals short-circuit' 'feature-envy parameter-by-value duplicate-code repeated-conditionals short-circuit'
+VARIANTS=('dead-local-store' 'duplicate-code' 'feature-envy' 'parameter-by-value' 'repeated-conditionals' 'self-assignment' 'short-circuit'
+           'type-checking' 'dead-code' 'redundant-data-storage' 'in-line' ''
+           'dead-local-store duplicate-code feature-envy parameter-by-value repeated-conditionals self-assignment short-circuit type-checking dead-code redundant-data-storage in-line' 'feature-envy parameter-by-value' 'feature-envy duplicate-code' 'feature-envy repeated-conditionals' 'feature-envy short-circuit' 'parameter-by-value duplicate code' 'parameter-by-value repeated-conditionals' 'parameter-by-value short-circuit' 'duplicate-code repeated-conditionals' 'duplicate-code short-circuit' 'repeated-conditionals short-circuit' 'feature-envy parameter-by-value duplicate-code' 'feature-envy parameter-by-value repeated-conditionals' 'feature-envy parameter-by-value short-circuit' 'feature-envy duplicate-code repeated-conditionals' 'feature-envy duplicate-code repeated-conditionals' 'feature-envy duplicate-code short-circuit' 'feature-envy repeated-conditionals short-circuit' 'parameter-by-value duplicate-code repeated-conditionals' 'parameter-by-value duplicate-code short-circuit' 'parameter-by-value repeated-conditionals short-circuit' 'duplicate-code repeated-conditionals short-circuit' 'feature-envy parameter-by-value duplicate-code repeated-conditionals' 'feature-envy parameter-by-value duplicate-code short-circuit' 'feature-envy parameter-by-value repeated-conditionals short-circuit' 'feature-envy duplicate-code repeated-conditionals short-circuit' 'parameter-by-value duplicate-code repeated-conditionals short-circuit' 'feature-envy parameter-by-value duplicate-code repeated-conditionals short-circuit'
            )
 # number of times to run each variant of each smell
-N=1
+N=400
 
 # time (in seconds) to sleep between each benchmark - that is, between each variant
 SLEEP=1
@@ -16,6 +18,7 @@ SMELL_BINARY_PATH=`realpath "binary/Smells"`
 
 # folder name of the current run - current time in seconds
 NOW=`date +%s`
+TWITCH=`realpath twitch.csv`
 mkdir "$NOW"
 cd "$NOW"
 
@@ -38,13 +41,15 @@ do
         echo "#!/bin/bash" >> "run.sh"
         echo "${SMELL_BINARY_PATH} ${smell} ${variant}" >> "run.sh"
         chmod +x "run.sh"
+        cp $TWITCH .
 
         RUN_PATH=`realpath "run.sh"`
         echo "Running code smell ${smell}, variant ${variant}"
 
         # run the benchmark
         # TODO: add -i with idle data immediately after "raplrs"
-        sudo raplrs -n "${smell}-${variant}" benchmark "$RUN_PATH" -n "${N}"
+	raplrs -n "${smell}-${variant}" benchmark "$RUN_PATH" -n "${N}"
+	echo "sudo raplrs -n "${smell}-${variant}" benchmark "$RUN_PATH" -n "${N}""
 
         # leave dir
         cd ..
