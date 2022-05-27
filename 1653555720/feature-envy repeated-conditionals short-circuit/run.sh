8ffb5e849cc6b824d982689bcc56659528d307d3
benchy-data <cs-21-pt-9-01@hotmail.com>
2022-05-27 07:14:18 +0000

    reran tests

---
+++ 1653555720/feature-envy repeated-conditionals short-circuit/run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/home/usr/Downloads/CodeSmells/binary/Smells super-smell feature-envy repeated-conditionals short-circuit
---
+++ 1653555720/parameter-by-value repeated-conditionals/run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/home/usr/Downloads/CodeSmells/binary/Smells super-smell parameter-by-value repeated-conditionals
